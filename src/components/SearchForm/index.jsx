c44df4fe12d5c776d2ed9ab6b079df0d9d40750c
Alberto Bautista <dachb101@gmail.com>
2022-07-23 12:19:20 -0500

    add search form component

---
+++ src/components/SearchForm/index.jsx
@@ -0,0 +1,169 @@
+import React from 'react';
+import { Grid, TextField, Button, Autocomplete } from '@mui/material';
+import moment from 'moment';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { airportsFiltered } from '../../data/airports';
+
+const validationSchema = yup.object({
+  originLocationCode: yup.string().required('Este campo es requerido'),
+  destinationLocationCode: yup.string().required('Este campo es requerido'),
+  departureDate: yup.string().required('Este campo es requerido'),
+  returnDate: yup.string(),
+  adults: yup
+    .number()
+    .min(1, 'Mínimo 1.')
+    .max(9, 'Máximo 9')
+    .integer('El valor debe ser un número entero')
+    .required('Este campo es requerido'),
+  children: yup.number().max(9, 'Máximo 9').integer('El valor debe ser un número entero')
+});
+
+export const SearchForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      originLocationCode: '',
+      destinationLocationCode: '',
+      departureDate: '',
+      returnDate: '',
+      adults: 1,
+      children: ''
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <Grid container spacing={2}>
+        <Grid item xs={2}>
+          <Autocomplete
+            disablePortal
+            id="originLocationCode"
+            options={airportsFiltered}
+            onChange={(e, value) => formik.setFieldValue('originLocationCode', value?.code || '')}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Origen"
+                error={
+                  formik.touched.originLocationCode && Boolean(formik.errors.originLocationCode)
+                }
+                helperText={formik.touched.originLocationCode && formik.errors.originLocationCode}
+              />
+            )}
+            size="small"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <Autocomplete
+            id="destinationLocationCode"
+            options={airportsFiltered}
+            onChange={(e, value) =>
+              formik.setFieldValue('destinationLocationCode', value?.code || '')
+            }
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Destino"
+                error={
+                  formik.touched.destinationLocationCode &&
+                  Boolean(formik.errors.destinationLocationCode)
+                }
+                helperText={
+                  formik.touched.destinationLocationCode && formik.errors.destinationLocationCode
+                }
+              />
+            )}
+            size="small"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <TextField
+            id="departureDate"
+            name="departureDate"
+            size="small"
+            type="date"
+            label="Ida"
+            inputProps={{
+              min: moment().format('YYYY-MM-DD')
+            }}
+            InputLabelProps={{
+              shrink: true
+            }}
+            fullWidth
+            value={formik.values.departureDate}
+            onChange={formik.handleChange}
+            error={formik.touched.departureDate && Boolean(formik.errors.departureDate)}
+            helperText={formik.touched.departureDate && formik.errors.departureDate}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <TextField
+            id="returnDate"
+            name="returnDate"
+            size="small"
+            type="date"
+            label="Regreso"
+            inputProps={{
+              min: moment().format('YYYY-MM-DD')
+            }}
+            InputLabelProps={{
+              shrink: true
+            }}
+            fullWidth
+            value={formik.values.returnDate}
+            onChange={formik.handleChange}
+            error={formik.touched.returnDate && Boolean(formik.errors.returnDate)}
+            helperText={formik.touched.returnDate && formik.errors.returnDate}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <TextField
+            id="adults"
+            name="adults"
+            size="small"
+            type="number"
+            label="Adultos"
+            inputProps={{
+              min: 1,
+              max: 9
+            }}
+            fullWidth
+            value={formik.values.adults}
+            onChange={formik.handleChange}
+            error={formik.touched.adults && Boolean(formik.errors.adults)}
+            helperText={formik.touched.adults && formik.errors.adults}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <TextField
+            id="children"
+            name="children"
+            size="small"
+            type="number"
+            label="Niños"
+            inputProps={{
+              min: 1,
+              max: 9
+            }}
+            fullWidth
+            value={formik.values.children}
+            onChange={formik.handleChange}
+            error={formik.touched.children && Boolean(formik.errors.children)}
+            helperText={formik.touched.children && formik.errors.children}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Button variant="contained" fullWidth type="submit">
+            Login
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
