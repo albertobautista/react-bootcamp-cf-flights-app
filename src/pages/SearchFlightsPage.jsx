6917fd12ad7d816887d9235efd8fc9fa13c12f31
Alberto Bautista <dachb101@gmail.com>
2022-07-23 12:20:13 -0500

    add search flights page

--- src/pages/SearchFlightsPage.jsx
+++ src/pages/SearchFlightsPage.jsx
@@ -1,7 +1,26 @@
+import { Grid, Typography } from '@mui/material';
 import React from 'react';
 
-const SearchFlightsPage = () => {
-  return <div>SearchFlightsPage</div>;
-};
+import { SearchForm } from '../components';
 
-export default SearchFlightsPage;
+export const SearchFlightsPage = () => {
+  return (
+    <Grid justifyContent="center" padding={5} sx={{ minHeight: '100vh' }}>
+      <Grid
+        item
+        border={5}
+        borderColor="purple"
+        className="shadow"
+        xs={3}
+        sx={{
+          padding: 3,
+          borderRadius: 2
+        }}>
+        <Typography variant="h6" fontWeight="bold" sx={{ mb: 1 }}>
+          Vuelos
+        </Typography>
+        <SearchForm />
+      </Grid>
+    </Grid>
+  );
+};
