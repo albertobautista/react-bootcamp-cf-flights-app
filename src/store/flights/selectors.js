3ff0ae6169255bf74b9e014facfaaf891ac3b56b
Alberto Bautista <dachb101@gmail.com>
2022-07-24 14:26:51 -0500

    add flights selectors

---
+++ src/store/flights/selectors.js
@@ -0,0 +1,5 @@
+export const isLoadingSelector = (state) => state.flights.isLoading;
+export const flightsCountSelector = (state) => state.flights.flightsCount;
+export const flightsResultsSelector = (state) => state.flights.flightsResults;
+export const flightsAircraftsSelector = (state) => state.flights.aircrafts;
+export const flightsCarriersSelector = (state) => state.flights.carriers;
