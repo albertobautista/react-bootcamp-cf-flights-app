be0a7c2c7b91dc8e8191ecf372b8ed60d316c753
Alberto Bautista <dachb101@gmail.com>
2022-07-24 14:28:51 -0500

    update FlightsResultsList component to use redux selectors

--- src/components/FlightsResultsList/index.jsx
+++ src/components/FlightsResultsList/index.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/prop-types */
 import { Typography } from '@mui/material';
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { flightsCountSelector } from '../../store/flights/selectors';
 import { FlightResultsItem } from '../FlightResultsItem';
 
 export const FlightsResultsList = ({ flights }) => {
   const [expanded, setExpanded] = useState(false);
+  const flightsCount = useSelector(flightsCountSelector);
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
@@ -12,7 +15,7 @@ export const FlightsResultsList = ({ flights }) => {
   return (
     <>
       <Typography variant="h6" fontWeight="bold" sx={{ mb: 1 }}>
-        Resultados: 1
+        Resultados: {flightsCount}
       </Typography>
       {flights.map((flight, index) => (
         <FlightResultsItem
