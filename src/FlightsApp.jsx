065bc63613e8d072c817f25ab1bfed6e843978bb
Alberto Bautista <dachb101@gmail.com>
2022-07-24 15:36:20 -0500

    getToken function

--- src/FlightsApp.jsx
+++ src/FlightsApp.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
+import { getToken } from './api/flightsApi';
 import AppRouter from './router/AppRouter';
 import { AppTheme } from './theme';
 
 const FlightsApp = () => {
+  useEffect(() => {
+    getToken();
+  }, []);
+
   return (
     <>
       <AppTheme>
--- src/api/flightsApi.js
+++ src/api/flightsApi.js
@@ -1,9 +1,19 @@
 import axios from 'axios';
 
+export const getToken = async () => {
+  const params = new URLSearchParams();
+  params.append('grant_type', 'client_credentials');
+  params.append('client_id', 'G9XmxXpuCohSWvLuyP3T0MLKT7vOfB2d');
+  params.append('client_secret', 'awSSXy0H8zLHtNaD');
+  const resp = await axios.post('https://test.api.amadeus.com/v1/security/oauth2/token', params);
+
+  localStorage.setItem('token', resp.data.access_token);
+};
+
 const flightsApi = axios.create({
   baseURL: 'https://test.api.amadeus.com/',
   headers: {
-    Authorization: `Bearer u8mfJ2gR4RmTwjHQtrnvnYDaIdQR`
+    Authorization: `Bearer ${localStorage.getItem('token')}`
   }
 });
 
