ecd2cd8b574dc739f179bc62db6624a71d011043
Alberto Bautista <dachb101@gmail.com>
2022-07-24 14:29:23 -0500

    update LoadingGif component

--- src/components/LoadingGif/index.jsx
+++ src/components/LoadingGif/index.jsx
@@ -2,14 +2,8 @@ import { CircularProgress, Grid } from '@mui/material';
 
 export const LoadingGif = () => {
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ padding: 5 }}>
-      <Grid item direction="row" justifyContent="center">
+    <Grid container alignItems="center" justifyContent="center" sx={{ padding: 5 }}>
+      <Grid item justifyContent="center">
         <CircularProgress color="primary" size="70px" />
       </Grid>
     </Grid>
