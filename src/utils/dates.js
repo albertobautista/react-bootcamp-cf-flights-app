bb4f26ed5a3bd7151d4fa34a0ad4fd908e0ca03a
Alberto Bautista <dachb101@gmail.com>
2022-07-23 20:55:59 -0500

    add dates utils

---
+++ src/utils/dates.js
@@ -0,0 +1,17 @@
+import moment from 'moment';
+
+export const getDate = (oldDate) => {
+  return moment(oldDate).format('DD/MM/YYYY');
+};
+
+export const getTime = (oldDate) => {
+  return moment(oldDate).format('HH:mm');
+};
+
+export const getDuration = (duration) => {
+  const days = moment.duration(duration).get('days');
+  const hours = moment.duration(duration).get('hours');
+  const minutes = moment.duration(duration).get('minutes');
+
+  return `${days > 0 ? days + ' d' : ''} ${hours} h ${minutes} m`;
+};
