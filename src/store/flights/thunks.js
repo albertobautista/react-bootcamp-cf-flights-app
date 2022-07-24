5fab4093a0eac77e9fb84c405b8f9157f8979467
Alberto Bautista <dachb101@gmail.com>
2022-07-24 14:27:13 -0500

    add flights thunks

---
+++ src/store/flights/thunks.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import flightsApi from '../../api/flightsApi';
+
+export const fetchFlights = createAsyncThunk(
+  'flights',
+  async (datos, { fulfillWithValue, rejectWithValue }) => {
+    try {
+      const {
+        originLocationCode,
+        destinationLocationCode,
+        departureDate,
+        returnDate,
+        adults,
+        children
+      } = datos;
+
+      const params = {
+        max: 15,
+        originLocationCode,
+        destinationLocationCode,
+        departureDate,
+        adults,
+        ...(children > 0 && { children }),
+        ...(returnDate !== '' && { returnDate })
+      };
+
+      const response = await flightsApi.get('/v2/shopping/flight-offers', {
+        params
+      });
+
+      console.log('RESPONSEEE', response);
+      return fulfillWithValue(response.data);
+    } catch (error) {
+      console.log('ERROR', error);
+      return rejectWithValue({
+        isError: true,
+        message: 'Error, try again later'
+      });
+    }
+  }
+);
