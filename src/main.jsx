5adff6452c857d14d6cb78f5a20f310cf24a8bc5
Alberto Bautista <dachb101@gmail.com>
2022-07-24 14:24:27 -0500

    add provider and store to main

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import FlightsApp from './FlightsApp';
 import './App.css';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <FlightsApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <FlightsApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
