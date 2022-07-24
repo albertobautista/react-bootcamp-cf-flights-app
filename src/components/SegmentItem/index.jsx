27ce332b8437cf52cf4a69803200bd46b38378cb
Alberto Bautista <dachb101@gmail.com>
2022-07-24 14:28:02 -0500

    update SegmentItem component to use redux selectors

--- src/components/SegmentItem/index.jsx
+++ src/components/SegmentItem/index.jsx
@@ -3,8 +3,13 @@ import { Grid, Typography } from '@mui/material';
 import React from 'react';
 import FlightTakeoffIcon from '@mui/icons-material/FlightTakeoff';
 import { getDate, getDuration, getTime } from '../../utils';
+import { useSelector } from 'react-redux';
+import { flightsAircraftsSelector, flightsCarriersSelector } from '../../store/flights/selectors';
 
 export const SegmentItem = ({ segment }) => {
+  const flightsAircrafts = useSelector(flightsAircraftsSelector);
+  const flightsCarriers = useSelector(flightsCarriersSelector);
+
   return (
     <Grid
       container
@@ -19,12 +24,12 @@ export const SegmentItem = ({ segment }) => {
         </Grid>
         <Grid item xs={2} sm={2} md={2}>
           <Typography fontWeight="bold" fontSize="13px">
-            {segment.carrierCode}
+            {flightsCarriers[segment.carrierCode]}
           </Typography>
         </Grid>
         <Grid item xs={8} sm={8} md={9} textAlign="right">
           <Typography fontWeight="bold" fontSize="13px">
-            Vuelo {segment.number} - Avion {segment.aircraft.code}
+            Vuelo {segment.number} - {flightsAircrafts[segment.aircraft.code]}
           </Typography>
         </Grid>
       </Grid>
@@ -35,7 +40,9 @@ export const SegmentItem = ({ segment }) => {
           </Typography>
           <Typography>{getDate(segment.departure.at)}</Typography>
           <Typography fontWeight="bold">{getTime(segment.departure.at)}</Typography>
-          <Typography>Terminal: {segment.departure.terminal}</Typography>
+          <Typography>
+            Terminal: {segment.departure.terminal ? segment.departure.terminal : 'Desconocida'}
+          </Typography>
         </Grid>
         <Grid item xs={12} sm={2} md={1} textAlign="center">
           <FlightTakeoffIcon fontSize="large" />
@@ -50,7 +57,9 @@ export const SegmentItem = ({ segment }) => {
           </Typography>
           <Typography>{getDate(segment.arrival.at)}</Typography>
           <Typography fontWeight="bold">{getTime(segment.arrival.at)}</Typography>
-          <Typography>Terminal: {segment.arrival.terminal}</Typography>
+          <Typography>
+            Terminal: {segment.arrival.terminal ? segment.arrival.terminal : 'Desconocida'}
+          </Typography>
         </Grid>
       </Grid>
     </Grid>
