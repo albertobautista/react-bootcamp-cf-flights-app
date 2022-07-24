572f4ac1af28482d7d6e58add5684823a59c7729
Alberto Bautista <dachb101@gmail.com>
2022-07-24 14:26:38 -0500

    add flights slice

---
+++ src/store/flights/flightsSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchFlights } from './thunks';
+
+export const flightsSlice = createSlice({
+  name: 'flights',
+  initialState: {
+    isLoading: false,
+    flightsResults: [],
+    aircrafts: {},
+    carriers: {},
+    fullResults: [],
+    flightsCount: 0,
+    error: {}
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchFlights.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchFlights.fulfilled, (state, action) => {
+        state.isLoading = false;
+        if (action.payload.data.length > 0) {
+          state.flightsResults = action.payload.data;
+          state.fullResults = action.payload;
+          state.aircrafts = action.payload.dictionaries.aircraft;
+          state.carriers = action.payload.dictionaries.carriers;
+          state.flightsCount = action.payload.meta.count;
+        }
+        state.error = {};
+      })
+      .addCase(fetchFlights.rejected, (state, action) => {
+        console.log('action', action);
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  }
+});
