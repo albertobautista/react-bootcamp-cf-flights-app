d4bdef1b9ccf6ff72150e178fb9863861e885942
Alberto Bautista <dachb101@gmail.com>
2022-07-23 21:00:39 -0500

    update SearchFlightsPage component in responsive mode

--- src/pages/SearchFlightsPage.jsx
+++ src/pages/SearchFlightsPage.jsx
@@ -1,25 +1,43 @@
 import { Grid, Typography } from '@mui/material';
 import React from 'react';
 
-import { SearchForm } from '../components';
+import { SearchForm, FlightsResultsList } from '../components';
+import { flightsData } from '../data/flights';
 
 export const SearchFlightsPage = () => {
   return (
-    <Grid justifyContent="center" padding={5} sx={{ minHeight: '100vh' }}>
+    <Grid
+      justifyContent="center"
+      padding={5}
+      sx={{ minHeight: '100vh', backgroundColor: '#f8f5f4' }}>
       <Grid
         item
-        border={5}
+        border={3}
         borderColor="purple"
         className="shadow"
         xs={3}
         sx={{
           padding: 3,
-          borderRadius: 2
+          borderRadius: 2,
+          backgroundColor: 'white'
         }}>
         <Typography variant="h6" fontWeight="bold" sx={{ mb: 1 }}>
           Vuelos
         </Typography>
         <SearchForm />
+      </Grid>
+      <Grid
+        item
+        xs={3}
+        sx={{
+          marginTop: 5
+        }}>
+        <Grid container justifyContent="center">
+          <Grid item xs={10}>
+            {/* <LoadingGif /> */}
+            <FlightsResultsList flights={flightsData} />
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
