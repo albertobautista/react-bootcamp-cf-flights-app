97cfad8dd3be8aae05ce70b97aab15c5bfe28bae
Alberto Bautista <dachb101@gmail.com>
2022-07-24 14:25:28 -0500

    add redux store

---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { flightsSlice } from './flights/flightsSlice';
+
+export const store = configureStore({
+  reducer: {
+    flights: flightsSlice.reducer
+  }
+});
